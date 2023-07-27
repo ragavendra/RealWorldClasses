c22f8070eb7ba1231bd8c30a30f9704b0fcfcdc5
Ragavendra <raga@example.com>
2023-07-27 12:40:57 -0700

    updated runner and load

--- src/RealWorldClasses/UseCases/AfceHummingBird.cs
+++ src/RealWorldClasses/UseCases/AfceHummingBird.cs
@@ -3,6 +3,42 @@ using RealWorldClasses.Birds;
 
 namespace RealWorldClasses.UseCases
 {
+    public class AfceBird
+    {
+        public static void Main()
+        {
+
+            // Create an humming bird named afceHummingBird in the respective location
+            Coordinates coordinates = new Coordinates() { Latitude = 3.161, Longitude = 1.69 };
+            AfceHummingBird afceHummingBird = new AfceHummingBird(coordinates);
+
+            // Create listeners near various co - ords near the afceHummingBird
+            Coordinates listenerCoordinates = new Coordinates() { Latitude = 3.161, Longitude = 1.69 };
+            new Listener(afceHummingBird, listenerCoordinates, "One");
+
+            try
+            {
+                // lets move this listener a bit further
+                listenerCoordinates.Latitude = listenerCoordinates.Latitude + .31;
+                new Listener(afceHummingBird, listenerCoordinates, "Two") { Radius = 6 };
+            }
+            catch (Exception ex)
+            { }
+
+            try
+            {
+                // lets move this listener a bit further
+                listenerCoordinates.Latitude = listenerCoordinates.Longitude - .62;
+                new Listener(afceHummingBird, listenerCoordinates, "Three") { Radius = 8 };
+            }
+            catch (Exception ex)
+            { }
+
+            afceHummingBird.GroakMessage("Hi");
+            afceHummingBird.TweetMessage("Hello");
+        }
+    }
+
     public sealed class AfceHummingBird : Hummingbird, IArea
     {
         private readonly Coordinates _location;
@@ -30,21 +66,46 @@ namespace RealWorldClasses.UseCases
         }
     }
 
-    public class Listener : IArea
+    public class Listener : IArea, IDisposable
     {
 
         private readonly Coordinates _location;
 
-        private readonly double _radius = 0.3;
+        private bool _disposed;
+
+        private string _name;
+
+        // the radius around which this listener can listen
+        private double _radius = 0.3;
 
         public double Radius
         {
             get => _radius;
+            set => _radius = value;
         }
 
-        public Listener(AfceHummingBird afceHummingBird, Coordinates location)
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                // disp managed resources
+                Dispose_();
+            }
+
+
+            // disp un managed resources
+            _disposed = true;
+        }
+
+        private void Dispose_()
+        {
+            // GC.SuppressFinalize(this);
+        }
+
+        public Listener(AfceHummingBird afceHummingBird, Coordinates location, string name)
         {
             _location = location;
+            _name = name;
 
             if(!NearMe(afceHummingBird.Location))
             {
@@ -91,23 +152,7 @@ namespace RealWorldClasses.UseCases
 
         void PrintMessage(string message)
         {
-            Console.WriteLine($"{GetType().Name} heard - {message}");
+            Console.WriteLine($"{GetType().Name} {_name} heard - {message}");
         }
     }
-
-    public class Listener2 : Listener
-    {
-        public Listener2(AfceHummingBird afceHummingBird, Coordinates location) : base(afceHummingBird, location)
-        {
-        }
-    }
-
-
-    public class Listener3 : Listener
-    {
-        public Listener3(AfceHummingBird afceHummingBird, Coordinates location) : base(afceHummingBird, location)
-        {
-        }
-    }
-
 }
---
+++ src/RealWorldClasses/UseCases/General.cs
@@ -0,0 +1,50 @@
+using RealWorldClasses;
+using RealWorldClasses.Birds;
+using RealWorldClasses.Kingdom.Animals;
+
+public class General
+{
+    public static void Main()
+    {
+        //turn on and listen to radio with 25 volume and fm 101.5
+        Radio radio = new Radio() { AmFm = amFm.AM };
+
+        // set to AM
+        // radio.AmFm = amFm.AM;
+
+        radio.IncreaseVolume = 1;
+
+        radio.Volume = 25;
+        radio.Station = 101.5;
+        radio.Station = 25;  //should not set
+        radio.Display();
+        Console.WriteLine();
+
+        Console.BackgroundColor = ConsoleColor.Magenta;
+        Bird hummingbird = new Hummingbird();
+
+        // Logger logger;
+        // hummingbird.EarsCount
+        Console.WriteLine($"{typeof(Hummingbird).Name} has - ");
+
+        foreach (var property in typeof(Hummingbird).GetProperties())
+        {
+            // property.GetValue(hummingbird);
+            Console.WriteLine($"{property.Name} is {property.GetValue(hummingbird)}");
+        }
+
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.WriteLine("---------");
+
+        Horse horse = new Horse();
+
+        // Parallel.ForEach(hummingbird.GetType().GetProperties(), type_ => Console.WriteLine($" is - {type_}"));
+        Console.WriteLine($"{typeof(Horse).Name} has - ");
+
+        foreach (var property in typeof(Horse).GetProperties())
+        {
+            // property.GetValue(hummingbird);
+            Console.WriteLine($"{property.Name} is {property.GetValue(horse)}");
+        }
+    }
+}
